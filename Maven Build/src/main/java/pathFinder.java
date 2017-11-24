6bc679ccf9a808a471e33b466a300dc87d501665
Anthony Boccadoro <aboccadoro@umass.edu>
2017-11-24 03:38:13 -0500

    workaround for URL mapping for testability

--- Maven Build/src/main/java/pathFinder.java
+++ Maven Build/src/main/java/pathFinder.java
@@ -2,19 +2,43 @@ import com.google.maps.*;
 import com.google.gson.*;
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 
 public class pathFinder {
     public static void main(String[] args) throws Exception {
-        //TODO Derive URL from keyboard input using addressing syntax and
-        URL request = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyASi8i_P2xPy-mlxEMurV_kWXBgQvVFyF4");
-        BufferedReader in = new BufferedReader(new InputStreamReader(request.openStream()));
-        //TODO Check the status of the output
+        //TODO Derive URL from geocoded keyboard input using addressing syntax (static+output?parameters)
+        String key = "&key=AIzaSyASi8i_P2xPy-mlxEMurV_kWXBgQvVFyF4";
+        URL stat = new URL("https://maps.googleapis.com/maps/api/geocode/");
+        BufferedReader buffer;
         String response;
+        //while() {
+            //Enter location to geoencode
+            Scanner input = new Scanner(System.in);
+            String in = input.nextLine();
+            char[] format = in.toCharArray();
+            int index = 0;
+            CharacterIterator it = new StringCharacterIterator(in);
+            for(char ch = it.first(); ch != CharacterIterator.DONE; ch = it.next()) {
+                if(ch == ' ') {
+                    format[index] = '+';
+                }
+                index++;
+            }
+            String address = "json?address=";
+            for(char ch : format) {
+                address += ch;
+            }
+            URL request = new URL(stat, address + key);
+            buffer = new BufferedReader(new InputStreamReader(request.openStream()));
+            //TODO Check the status of the output
+        //}
         //TODO Implement exponential backoff
-        while ((response = in.readLine()) != null) {
+        while ((response = buffer.readLine()) != null) {
             //TODO Parse the output
             System.out.println(response);
         }
-        in.close();
+        buffer.close();
     }
 }
