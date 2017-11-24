6394d37727fc41663d71e2956e1090a2378dc038
Anthony Boccadoro <aboccadoro@umass.edu>
2017-11-24 01:06:54 -0500

    simple location request that prints JSON response

--- Maven Build/src/main/java/pathFinder.java
+++ Maven Build/src/main/java/pathFinder.java
@@ -5,11 +5,16 @@ import java.io.*;
 
 public class pathFinder {
     public static void main(String[] args) throws Exception {
-        URL request = new URL("");
+        //TODO Derive URL from keyboard input using addressing syntax and
+        URL request = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyASi8i_P2xPy-mlxEMurV_kWXBgQvVFyF4");
         BufferedReader in = new BufferedReader(new InputStreamReader(request.openStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
+        //TODO Check the status of the output
+        String response;
+        //TODO Implement exponential backoff
+        while ((response = in.readLine()) != null) {
+            //TODO Parse the output
+            System.out.println(response);
+        }
         in.close();
     }
 }
