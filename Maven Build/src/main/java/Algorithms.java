ada51a0145ac881704b49af7891935c600bdf2dc
Anthony Boccadoro <aboccadoro@umass.edu>
2017-12-05 18:25:21 -0500

    fixed dependency issues in pom.xml

--- Maven Build/src/main/java/Algorithms.java
+++ Maven Build/src/main/java/Algorithms.java
@@ -1,5 +1,3 @@
-package cs520EleNa;
-
 // A Java program for Dijkstra's single source shortest path algorithm.
 // The program is for adjacency matrix representation of the graph
 import java.util.*;
